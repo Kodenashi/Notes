e7c8e3861f8306ad752371ecb58d63821a32bd5a
DJ <jancelleopoldo@gmail.com>
2025-06-28 04:08:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function copyText(noteId) {
   const text = document.getElementById(noteId).innerText;
-    navigator.clipboard.writeText(text).then(() => {
-      alert('Note copied to clipboard!');
-    }).catch(err => {
-      alert('Failed to copy text: ' + err);
-    });
+  navigator.clipboard.writeText(text).then(() => {
+    alert('Note copied to clipboard!');
+  }).catch(err => {
+    alert('Failed to copy text: ' + err);
+  });
 }
